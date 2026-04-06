8380d3dffefc4e19c07ea8d129c3054ff1c064c4
Abubakar Orakzai <abubakarorakzai15@gamail.com>
2026-04-06 17:02:59 +0500

    Adding the component_and_props folder.
    Signed-off-by: Abubakar Orakzai <abubakarorakzai15@gamail.com>

---
+++ Component_and_props/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Navbar.css'
+function Navbar() {
+  return (
+    <> 
+    <div className='navbar'>
+        <a href="Home">Home</a>
+        <a href="Home">About Us</a>
+        <a href="Home">Contact</a>
+    </div>
+    </>
+  )
+}
+
+export default Navbar
---
+++ react_project_02/src/orakzai.jsx
@@ -0,0 +1,12 @@
+function Orakzai()
+{
+    return(
+     <div>
+        <h1>Abubakar Orakzai</h1>
+         <h2>A great Software Engineer who love to manipluate Dom </h2>
+    </div>
+    )
+}
+
+
+export default Orakzai;
