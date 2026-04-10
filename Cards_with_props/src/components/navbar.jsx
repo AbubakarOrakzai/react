6637cd37ff4fce5962cf2fdde42deab1fae5c1a3
Abubakar Orakzai <abubakarorakzai15@gamail.com>
2026-04-10 19:50:36 +0500

    Adding the Cards_with_props
    Signed-off-by: Abubakar Orakzai <abubakarorakzai15@gamail.com>

--- Cards_with_props/src/components/navbar.jsx
+++ Cards_with_props/src/components/navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
     <div>
         <div className='Navbar'>
             <h1>My Website</h1>
-              <ul>
+        <ul>
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#contact">Contact</a></li>
